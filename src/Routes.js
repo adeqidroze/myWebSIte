acd7e860e154295dad13d0298598115568a425cd
adeqidroze <adeqidroze@gmail.com>
2023-11-11 01:15:57 +0400

    fix

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ClientAccountPage from './pages/ClientAccountPage';
 
 
-export const myRoutes = () => {
+export const MyRoutes = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login-page" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/account/:clientId" element={<ClientAccountPage />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login-page" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/account/:clientId" element={<ClientAccountPage />} />
+        </Routes>
     );
 }
